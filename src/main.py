94117c848a95aa7e9cea7253dc5181886f2d7e89
Julia Hoge <17hogeju@gmail.com>
2022-11-11 19:09:40 -0800

    Update code

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ import csv
 
 TRAIN_DATA_PATH = '../data/train.csv'
 TEST_DATA_PATH = '../data/test.csv'
-NUM_TRAIN = 1500
-NUM_TEST = 500
+NUM_TRAIN = 100
+NUM_TEST = 50
 RANGE = 1000
-EPOCHS = 500
+EPOCHS = 100
 W0 = -2
 W1 = 1
 W2 = 3
@@ -17,19 +17,26 @@ CLASS = 'class'
 
 
 class Perceptron:
-    def __init__(self) -> None:
-        self.weights = np.array([[W0],[W1],[W2]])
+    def __init__(self, train: np.ndarray, test: np.ndarray, weights) -> None:
+        self.train = train
+        self.test = test
+        self.weights = weights
+    
+    def batch_update_weights(self, lrate):
+        for i in range(self.train.shape[0]):
+            row_x = self.train[[i],:-1]
+            row_y = self.train[i,-1]
+            row_y = row_y.reshape((1,1))
+            sign = np.dot(row_x, self.weights)
+            sign = np.where(sign > 0, 1, -1)
+            diff = np.subtract(row_y, sign)
+            delta_weights = np.multiply((lrate * diff), row_x)
+            delta_weights = np.transpose(delta_weights)
+            self.weights = self.weights + delta_weights
 
-    def batch_update_weights(self, train_x: np.ndarray, train_y: np.ndarray, lrate):
-        for i in range(len(self.weights)):
-            o = np.dot(self.weights, train_x)
-            grad_desc = sum(np.subtract(train_y, o) * -(train_x[:,[i]]))
-            delta_weight = -lrate * grad_desc
-            self.weights[i] += delta_weight
-
-    def mse(self, test_x: np.ndarray, test_y: np.ndarray):
-        prediction = np.dot(self.weights, test_x)
-        diff = np.subtract(test_y - prediction)
+    def mse(self):
+        prediction = np.dot(self.test[:,:-1], self.weights)
+        diff = np.subtract(self.test[:,[-1]], prediction)
         return np.sum(diff**2)
 
 
@@ -42,28 +49,28 @@ def generate_data(num_examples):
     x2 = np.random.uniform(-RANGE, RANGE, size=(num_examples, 1))
     x_data = np.concatenate((x0, x1, x2), axis=1)
     cl = np.dot(x_data, np.array([[W0],[W1],[W2]]))
+    cl = np.where(cl > 0, 1, -1)
     data = np.concatenate((x_data, cl), axis=1)
-    with open(TRAIN_DATA_PATH, 'w', newline='') as f:
-        writer = csv.writer(f)
+    with open(TRAIN_DATA_PATH, 'w', newline='') as f1:
+        writer = csv.writer(f1)
         writer.writerows(data[:NUM_TRAIN])
-    with open(TEST_DATA_PATH, 'w', newline='') as f:
-        writer = csv.writer(f)
+    with open(TEST_DATA_PATH, 'w', newline='') as f2:
+        writer = csv.writer(f2)
         writer.writerows(data[NUM_TRAIN:])
 
 def main():
     print("Assignment 3: Naive Bayes")
     generate_data(NUM_TRAIN+NUM_TEST)
     train_data = np.genfromtxt(TRAIN_DATA_PATH, delimiter=',')
-    train_x = train_data[:,:-1]
-    train_y = train_data[:, [-1]]
-
     test_data = np.genfromtxt(TEST_DATA_PATH, delimiter=',')
-    test_x = test_data[:,:-1]
-    test_y = test_data[:, [-1]]
 
-    percep = Perceptron()
-    # for epoch in range(EPOCHS):
-    #     percep.batch_update_weights(train_x, train_y, .1)
+    weights = np.random.uniform(-RANGE, RANGE, size=(3, 1))
+
+    percep = Perceptron(train_data, test_data, weights)
+    for epoch in range(EPOCHS):
+        percep.batch_update_weights(0.1)
+        print(percep.mse())
+    print(percep.weights)
 
 
                   
