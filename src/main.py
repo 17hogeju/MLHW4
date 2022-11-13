b06f0a0bcab6ee50cb532186418064110e80aa6c
Julia Hoge <17hogeju@gmail.com>
2022-11-12 17:24:22 -0800

    Add plot labels

--- src/main.py
+++ src/main.py
@@ -24,8 +24,18 @@ class Perceptron:
         self.test = test
         self.weights = weights
     
+    def calculate_error(self):
+        wrong = 0
+        for i in range(self.test.shape[0]):
+            row_x = self.test[[i],:-1]
+            row_y = self.test[i,-1]
+            row_y = row_y.reshape((1,1))
+            diff = np.dot(row_x, self.weights)
+            if row_y*diff < 0:
+                wrong += 1
+        return wrong/self.test.shape[0]
+
     def incremental_update_weights(self, lrate):
-        epoch_error = 0
         for i in range(self.train.shape[0]):
             row_x = self.train[[i],:-1]
             row_y = self.train[i,-1]
@@ -36,9 +46,6 @@ class Perceptron:
             diff = diff * lrate
             delta_weights = np.dot(np.transpose(row_x), diff)
             self.weights = self.weights + delta_weights
-            if (row_y * sign) < 0:
-                epoch_error += 1
-        return (epoch_error/self.train.shape[0])
 
     def batch_update_weights(self, lrate):
         x = self.train[:,:-1]
@@ -49,13 +56,6 @@ class Perceptron:
         diff = diff * lrate
         delta_weights = np.dot(np.transpose(x), diff)
         self.weights = self.weights + delta_weights
-        
-        epoch_error = 0
-        for i in range(self.train.shape[0]):
-            if (int(y[i]) * int(sign[i])) < 0:
-                epoch_error += 1
-
-        return(epoch_error/self.train.shape[0])
 
 def generate_data(num_examples):
     print(f'Generating data in {TRAIN_DATA_PATH} and {TEST_DATA_PATH}')
@@ -98,15 +98,16 @@ def plotDecSurf(epoch, weights, train, type):
     x2_min = int(np.ceil(np.amin(train[:, 2])))
     
     fig = plt.figure()
-    plt.scatter(train[pos, 1], train[pos, 2], c ="r")
-    plt.scatter(train[neg, 1], train[neg, 2], c ="b")
-    plt.plot([x1_min, x1_max], [(2-x1_min)/3, (2-x1_max)/3],c="k")
+    plt.scatter(train[pos, 1], train[pos, 2], c ="r", label='Positive Class')
+    plt.scatter(train[neg, 1], train[neg, 2], c ="b", label='Negative Class')
+    plt.plot([x1_min, x1_max], [(2-x1_min)/3, (2-x1_max)/3],c="k", label='Decision Line')
     plt.axvline(x=0, c="lightgray", linestyle="dashed", label="x=0")
     plt.axhline(y=0, c="lightgray", linestyle="dashed", label="y=0")
     plt.xlim([x1_min, x1_max])
     plt.ylim([x2_min, x2_max])
     plt.xlabel('x1')
     plt.ylabel('x2')
+    plt.legend(loc='upper right')
     plt.title('Decision Surface After Epoch = '+ str(epoch) + " ("+type+")")
     fig.savefig(FIGURES_PATH + type+"/dec_surf"+str(epoch)+".png")
     plt.clf()
@@ -120,55 +121,62 @@ def main():
 
     weights = np.random.uniform(-10, 10, size=(3, 1))
 
+    # Decision surface plots only on eta = 0.1
+    #                               ***
     eta_lst = [0.0001, 0.001, 0.01, 0.1, 0.5]
-    
     err_inc = []
     err_batch = []
+
+    # Problem 1
     for eta in eta_lst:
+        # Delta training for incremental
         err = []
         percep_inc = Perceptron(train_data, test_data, weights)
         for epoch in range(EPOCHS):
-            err.append(percep_inc.incremental_update_weights(eta))
+            percep_inc.incremental_update_weights(eta)
+            err.append(percep_inc.calculate_error())
             if eta == 0.1:
                 if (epoch == 4 or epoch == 9 or epoch == 49 or epoch == 99):
                     plotDecSurf(epoch, percep_inc.weights, percep_inc.train, "incremental")
-        
         err_inc.append(err)
         
+        # Delta training for batch
         err = []
         percep_batch = Perceptron(train_data, test_data, weights)
         for epoch in range(EPOCHS):
-            err.append(percep_batch.batch_update_weights(eta))
+            percep_batch.batch_update_weights(eta)
+            err.append(percep_batch.calculate_error())
             if eta == 0.1:
                 if (epoch == 4 or epoch == 9 or epoch == 49 or epoch == 99):
                     plotDecSurf(epoch, percep_batch.weights, percep_batch.train, "batch")
 
         err_batch.append(err)
-
     plotError(err_inc, "incremental", eta_lst)
     plotError(err_batch, "batch", eta_lst)
 
-    # Decaying learning rate
+    # Problem 2
+    # Decaying learning rate for incremental
     err = []
     eta_decay = 0.1
     percep_decay_inc = Perceptron(train_data, test_data, weights)
     for epoch in range(EPOCHS):
-        err.append(percep_decay_inc.incremental_update_weights(eta_decay))
+        percep_decay_inc.incremental_update_weights(eta_decay)
+        err.append(percep_decay_inc.calculate_error())
         eta_decay *= 0.8
         if (epoch == 4 or epoch == 9 or epoch == 49 or epoch == 99):
             plotDecSurf(epoch, percep_decay_inc.weights, percep_decay_inc.train, "decay_inc")
-
     plotError([err, err_inc[3]], "decay_inc", ["decay", "normal"])
-
+    
+    # Decaying learning rate for batch
     err = []
     eta_decay = 0.1
     percep_decay_batch = Perceptron(train_data, test_data, weights)
     for epoch in range(EPOCHS):
-        err.append(percep_decay_batch.incremental_update_weights(eta_decay))
+        percep_decay_batch.incremental_update_weights(eta_decay)
+        err.append(percep_decay_batch.calculate_error())
         eta_decay *= 0.8
         if (epoch == 4 or epoch == 9 or epoch == 49 or epoch == 99):
             plotDecSurf(epoch, percep_decay_batch.weights, percep_decay_batch.train, "decay_batch")
-
     plotError([err, err_batch[3]], "decay_batch", ["decay", "normal"])
 
 
